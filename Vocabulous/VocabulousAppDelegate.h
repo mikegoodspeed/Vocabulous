809d0afb3aac06fcc82e414bd512035fef331f21
Mike Goodspeed <mikegoodspeed@gmail.com>
2011-07-02 09:53:48 +0100

    Initial Commit

---
+++ Vocabulous/VocabulousAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  VocabulousAppDelegate.h
+//  Vocabulous
+//
+//  Created by Mike Goodspeed on 7/2/11.
+//  Copyright 2011 Mike Goodspeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VocabulousAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
